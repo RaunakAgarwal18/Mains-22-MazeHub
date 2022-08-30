71b23b6b60d43105fd539d701236c6e1b8f5eb82
chelsi-001011 <chelsirawat13@gmail.com>
2022-08-31 01:48:12 +0530

    PROD changes

--- cornhub/settings.py
+++ cornhub/settings.py
@@ -9,11 +9,19 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
 
-from pathlib import Path
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +33,7 @@ SECRET_KEY = 'django-insecure-07ku6#9vx0g#+nb5x#-)z5fm^$&qgn8=$l)8=i9o_i04a-=09*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
 ]
 
 MIDDLEWARE = [
@@ -71,9 +78,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cornhub.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+#DATABASES = {"default": env.db("DATABASE_URL", "sqlite:///sqlite3.db")}
 
 DATABASES = {
     'default': {
@@ -119,10 +127,13 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL='1'
 LOGIN_URL='login'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_ROOT = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
