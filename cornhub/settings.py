856f3137e0b6f9f34548dc325438987f8ca0370e
chelsi-001011 <chelsirawat13@gmail.com>
2022-09-01 12:48:59 +0530

    Db update

--- cornhub/settings.py
+++ cornhub/settings.py
@@ -75,17 +75,23 @@ WSGI_APPLICATION = 'cornhub.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chub22',
-        'USER':'techno',
-        'PASSWORD':'zGB%xVCAO9Z6',
-        'HOST':'localhost',
-        'PORT':''
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'chub22',
+#         'USER':'techno',
+#         'PASSWORD':'zGB%xVCAO9Z6',
+#         'HOST':'localhost',
+#         'PORT':''
+#     }
+# }
 
 
 # Password validation
